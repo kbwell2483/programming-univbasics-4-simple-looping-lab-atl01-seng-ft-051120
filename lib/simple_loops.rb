6a03f001d4f907234301760989a07d06b3646598
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-05-07 14:29:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,8 @@
-def loop_message_five_times (message)
-  count = 0 
-  while count < 5 do 
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
     puts message
-    count += 1 
+    count += 1
   end
 end
 
